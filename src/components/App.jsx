8dc59f290d7e26368197c168cf48868fba0777b2
Ravindra R. Jaju <jaju@msync.org>
2018-06-15 21:48:30 +0530

    Added material-ui, and a button component from it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 export default class App extends React.Component {
     render() {
 	return(
 	    <div style={{textAlign: 'center'}}>
-	    <h1>Helloy, World!</h1>
+	    <h1>Hello, World!</h1>
+	    <Button variant="contained" color="primary">
+	      Hello, Button!
+	    </Button>
 	    </div>);
     }
 }
